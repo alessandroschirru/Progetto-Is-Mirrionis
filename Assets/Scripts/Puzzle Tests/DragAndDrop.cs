b4a2ff009d3d259bdcbb68145db9b77f32764a34
alessandroschirru <schirrualeas3@gmail.com>
2025-07-27 17:17:41 +0200

    Puzzle monete sistemato

--- Assets/Scripts/Puzzle Tests/DragAndDrop.cs
+++ Assets/Scripts/Puzzle Tests/DragAndDrop.cs
@@ -2,62 +2,79 @@
 
 public class DragAndDrop : MonoBehaviour
 {
-    private bool isDragging = false;
     private Vector3 offset;
     private Vector3 startPosition;
+    private bool isDragging = false;
 
-    private void OnMouseDown()
+    // Layer da considerare per il drag (solo le monete)
+    public LayerMask draggableLayer;
+
+    void Update()
     {
-        this.transform.GetComponent<CircleCollider2D>().enabled = true;
-        isDragging = true;
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        startPosition = transform.position;
-
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        offset = transform.position - new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z);
-        Debug.Log("preso");
-    }
-
-    private void OnMouseDrag()
-    {
-        if (isDragging)
+        // Inizio drag
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z) + offset;
+            Collider2D hit = Physics2D.OverlapPoint(mouseWorldPos, draggableLayer);
+
+            if (hit != null && hit.gameObject == gameObject)
+            {
+                StartDrag(mouseWorldPos);
+            }
+        }
+
+        // Durante drag
+        if (isDragging && Input.GetMouseButton(0))
+        {
+            Drag(mouseWorldPos);
+        }
+
+        // Fine drag
+        if (isDragging && Input.GetMouseButtonUp(0))
+        {
+            EndDrag(mouseWorldPos);
         }
     }
 
-    private void OnMouseUp()
+    void StartDrag(Vector2 mousePos)
     {
-        Debug.Log("Ciao");
+        startPosition = transform.position;
+        offset = transform.position - new Vector3(mousePos.x, mousePos.y, transform.position.z);
+        isDragging = true;
+    }
+
+    void Drag(Vector2 mousePos)
+    {
+        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z) + offset;
+    }
+
+    void EndDrag(Vector2 mousePos)
+    {
         isDragging = false;
 
-        this.transform.GetComponent<CircleCollider2D>().enabled = false;
+        Collider2D[] hits = Physics2D.OverlapPointAll(mousePos);
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Collider2D hit = Physics2D.OverlapPoint(mousePos);
+        bool onCoinSpot = false;
+        bool onTable = false;
 
-        if (hit == null)
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.CompareTag("CoinSpot"))
+            {
+                transform.position = hit.transform.position;
+                onCoinSpot = true;
+                break;
+            }
+            else if (hit.CompareTag("Table"))
+            {
+                onTable = true;
+            }
+        }
+
+        if (!onCoinSpot && !onTable)
         {
             transform.position = startPosition;
         }
-        else if (hit.CompareTag("CoinSpot"))
-        {
-            transform.position = hit.transform.position;
-        }
-
-        this.transform.GetComponent<CircleCollider2D>().enabled = true;
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
